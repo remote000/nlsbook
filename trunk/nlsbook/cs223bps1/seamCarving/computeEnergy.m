2a35a6eae3211218642c7edacb164a6c737b6467
bridger.zhao <bridger.zhao@495d6f2b-cd45-5456-95d0-73ea48696b8d>
2011-01-11 11:09:01 +0000

    223bps1 finished

--- trunk/nlsbook/cs223bps1/seamCarving/computeEnergy.m
+++ trunk/nlsbook/cs223bps1/seamCarving/computeEnergy.m
@@ -5,3 +5,9 @@ function E = ComputeEnergy(img)
 %   statement
 %INPUTS:
 %   img: The RGB representation of the loaded image
+sobely=[1,2,1;0,0,0;-1,-2,-1];
+sobelx=[1,0,-1;2,0,-2;1,0,-1];
+E=zeros(size(img,1),size(img,2));
+for i=1:3
+    E=E+abs(filter2(sobely,img(:,:,i)))+abs(filter2(sobelx,img(:,:,i)));
+end
--- trunk/nlsbook/cs223bps1/seamCarving/findSeam_Greedy.m
+++ trunk/nlsbook/cs223bps1/seamCarving/findSeam_Greedy.m
@@ -1,7 +1,25 @@
-function pix = findSeam_Greedy(energy)
+function [pix,mine] = findSeam_Greedy(energy)
 %FINDSEAM GREEDY: Part B(i)
 %INPUT:
 %   energy: the 2D matrix of image energy that you calculated in part (a)
 %OUTPUT: 
 %   pix is a 1-D array of the column indices in the seam to be removed
-
+pix=zeros(size(energy,1),1);
+minp=zeros(size(energy,1),1);
+nc=size(energy,2);
+[minp(1),pix(1)]=min(energy(1,:));
+for i=2:size(energy,1)
+    prev=pix(i-1);
+    if prev==1
+        [minp(i),pix(i)]=min(energy(i,1:2));
+        continue;
+    end
+    if prev==nc
+        [minp(i),pix(i)]=min(energy(i,nc-1:nc));
+        pix(i)=nc-2+pix(i);
+        continue;
+    end
+    [minp(i),pix(i)]=min(energy(i,prev-1:prev+1));
+    pix(i)=prev-2+pix(i);
+end
+mine=sum(minp);
