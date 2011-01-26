0d3a6800f5cf06a1d25bcdbb679e40f752787b7b
bridger.zhao <bridger.zhao@495d6f2b-cd45-5456-95d0-73ea48696b8d>
2011-01-26 11:43:55 +0000

    remove check board features for can video

--- trunk/nlsbook/sfa/slowness_model/simulation2.m
+++ trunk/nlsbook/sfa/slowness_model/simulation2.m
@@ -37,7 +37,7 @@ function hdl = simulation(input_dim, imgs, varargin),
 %   See also: IMGSEQUENCE
   
   %%%% default values
-  load bin_120r_notrack;
+  load can_120row_notrack;
   data2=data;
   clear data;  
   % number of slowly varying functions to keep
